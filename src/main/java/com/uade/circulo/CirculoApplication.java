709638e8d3bc691535513f2c8054578efdc8f00d
Napero <69263168+Napero@users.noreply.github.com>
2025-08-30 14:16:37 -0300

    nombre de proyecto

--- src/main/java/com/uade/circulo/CirculoApplication.java
+++ src/main/java/com/uade/circulo/CirculoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.uade.circulo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CirculoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CirculoApplication.class, args);
 	}
 
 }
--- src/test/java/com/uade/circulo/CirculoApplicationTests.java
+++ src/test/java/com/uade/circulo/CirculoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.uade.circulo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CirculoApplicationTests {
 
 	@Test
 	void contextLoads() {
