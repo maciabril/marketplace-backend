491c366a0db3a3736310f25bc7f034c7ca422bf3
Napero <69263168+Napero@users.noreply.github.com>
2025-09-07 20:26:31 -0300

    jsonbackreference

--- src/main/java/com/uade/circulo/entity/User.java
+++ src/main/java/com/uade/circulo/entity/User.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -42,8 +44,8 @@ public class User implements UserDetails {
     @Column(nullable = false, unique = true)
     private String lastName;
 
-    // la profesora lo hizo así
     @OneToMany(mappedBy = "user")
+    @JsonManagedReference
     private List<Order> orders;
 
     @Enumerated(EnumType.STRING)
